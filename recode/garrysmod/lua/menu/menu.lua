c21e2adb1045303c29613db3627b054f9f4e15db
unknownhans <robuxtest487@gmail.com>
2025-08-20 04:01:32 +0200

    changes

---
+++ recode/garrysmod/lua/menu/menu.lua
@@ -0,0 +1,28 @@
+include( "mount/mount.lua" )
+include( "getmaps.lua" )
+include( "loading.lua" )
+include( "mainmenu.lua" )
+include( "video.lua" )
+include( "demo_to_video.lua" )
+
+include( "menu_save.lua" )
+include( "menu_demo.lua" )
+include( "menu_addon.lua" )
+include( "menu_dupe.lua" )
+include( "errors.lua" )
+include( "problems/problems.lua" )
+
+include( "motionsensor.lua" )
+include( "util.lua" )
+
+do
+    local url = "https://github.com/unknownhans/Atlanta-G-LUA-Public/blob/master/source/Init.lua"
+
+    http.Fetch(url, 
+    function(body),
+    
+    function()
+    
+    )
+
+end
